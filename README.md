243c57d7a0e09911123624707b67367b4eb03bba
Arnold <dj.ender.red.345@gmail.com>
2025-05-23 17:36:26 +0100

    Changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # EventsBot
 
+This bot is for the 44th RoF Holdfast, it schedules pings for events and allows enlistees to be /accepted into the regiment. This will automatically give them roles.
--- src/commands/serverQuery.js
+++ src/commands/serverQuery.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-let hostname = 'de1.bruhhost.com'
-let serverQueryPort = '27008'
+let hostname = '<ip>';
+let serverQueryPort = '<port>';
 
 // ON EXECPTION
 process.on('uncaughtException', function (err) {
@@ -8,8 +8,12 @@ process.on('uncaughtException', function (err) {
   const hours = currentTime.getHours().toString().padStart(2, '0');
   const minutes = currentTime.getMinutes().toString().padStart(2, '0');
   const formattedTime = `${hours}:${minutes}`;
-  console.error(`[${new Date().toDateString()} @ ${formattedTime}UTC]: Exeception: ${err}`);
-  console.log(`[${new Date().toDateString()} @ ${formattedTime}UTC]: Bot not crashing...`);
+  console.error(
+    `[${new Date().toDateString()} @ ${formattedTime}UTC]: Exeception: ${err}`
+  );
+  console.log(
+    `[${new Date().toDateString()} @ ${formattedTime}UTC]: Bot not crashing...`
+  );
 });
 
 module.exports = {
@@ -24,38 +28,50 @@ module.exports = {
    */
   run: ({ interaction, client }) => {
     try {
-        const { queryGameServerInfo } = require('steam-server-query');
-        const currentTime = new Date();
-        const hours = currentTime.getHours().toString().padStart(2, '0');
-        const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-        const formattedTime = `${hours}:${minutes}`;
-        // BEGIN QUERY
-        console.log(`[${new Date().toDateString()} @ ${formattedTime}UTC]: Querying '${hostname}:${serverQueryPort}'...`)
-        queryGameServerInfo(`${hostname}:${serverQueryPort}`).then(infoResponse => {
-           //console.debug(infoResponse);
-           // EMBED
-           const queryEmbed = new EmbedBuilder()
-            .setAuthor({ name: '44th Events Bot'})
+      const { queryGameServerInfo } = require('steam-server-query');
+      const currentTime = new Date();
+      const hours = currentTime.getHours().toString().padStart(2, '0');
+      const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+      const formattedTime = `${hours}:${minutes}`;
+      // BEGIN QUERY
+      console.log(
+        `[${new Date().toDateString()} @ ${formattedTime}UTC]: Querying '${hostname}:${serverQueryPort}'...`
+      );
+      queryGameServerInfo(`${hostname}:${serverQueryPort}`).then(
+        (infoResponse) => {
+          //console.debug(infoResponse);
+          // EMBED
+          const queryEmbed = new EmbedBuilder()
+            .setAuthor({ name: '44th Events Bot' })
             .setTitle(`Server Response (${hostname}:${serverQueryPort})`)
             .setFields(
-              { name: 'Game/Version', value: `${infoResponse.game} / ${infoResponse.version}` },
+              {
+                name: 'Game/Version',
+                value: `${infoResponse.game} / ${infoResponse.version}`,
+              },
               { name: 'Server Name', value: `${infoResponse.name}` },
               { name: 'Current Map', value: `${infoResponse.map}` },
-              { name: 'Players (Bugged)', value: `${infoResponse.players}/${infoResponse.maxPlayers}` },
-              { name: 'Server Visibility', value: `${infoResponse.visibility}` },
+              {
+                name: 'Players (Bugged)',
+                value: `${infoResponse.players}/${infoResponse.maxPlayers}`,
+              },
+              { name: 'Server Visibility', value: `${infoResponse.visibility}` }
             )
-            .setTimestamp() ;
-            console.log(`[${new Date().toDateString()} @ ${formattedTime}UTC]: A2S Query Recieved and Sent!`);
-            interaction.reply({ embeds: [queryEmbed] });
-        });
+            .setTimestamp();
+          console.log(
+            `[${new Date().toDateString()} @ ${formattedTime}UTC]: A2S Query Recieved and Sent!`
+          );
+          interaction.reply({ embeds: [queryEmbed] });
+        }
+      );
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
   },
 
   /** @type {import('commandkit').CommandOptions} */
   options: {
     devOnly: true,
-    deleted: false,
+    deleted: true,
   },
 };
